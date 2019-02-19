f664d9e8abcd8ce1207a275d642bf154d45b5b43
Marinus Klasen <marinus@mklasen.nl>
2019-02-19 14:43:05 +0100

    Use street address from woocommerce

--- radish-separate-wc-address.php
+++ radish-separate-wc-address.php
@@ -37,11 +37,11 @@ class Radish_Checkout_Fields {
 	function __construct() {
 		$this->init();
 	}
-
+	
 	public function init() {
 		add_filter( 'woocommerce_billing_fields', array( $this, 'alter_billing_checkout_fields' ), 20 );
 		add_filter( 'woocommerce_shipping_fields', array( $this, 'alter_shipping_checkout_fields' ), 20 );
-
+		
 		if ( ! is_plugin_active( 'wc-postcode-checker/wc-postcode-checker.php' ) ) {
 			add_action( 'woocommerce_checkout_update_order_meta', array( $this, 'process_fields' ) );
 			add_action( 'woocommerce_admin_order_data_after_billing_address', array(
@@ -53,60 +53,63 @@ class Radish_Checkout_Fields {
 				'show_extra_shipping_fields'
 			) );
 		}
-
+		
 		add_filter( 'woocommerce_checkout_fields', array( $this, 'checkout_move_country_field' ), 20 );
-
+		
 		add_filter( 'woocommerce_default_address_fields', array( $this, 'checkout_change_address_1_placeholder' ), 20 );
-
+		
 		add_filter( 'wpo_wcnlpc_postcode_field_countries', function ( $country_codes ) {
 			$countries = new \WC_Countries();
 			$countries = $countries->__get( 'countries' );
-
+			
 			foreach ( $countries as $code => $country ) {
 				$country_codes[] = $code;
 			}
-
+			
 			return $country_codes;
 		} );
 	}
-
+	
 	public function alter_billing_checkout_fields( $fields ) {
 		return $this->alter_checkout_fields( $fields, 'billing' );
 	}
-
+	
 	public function alter_shipping_checkout_fields( $fields ) {
 		return $this->alter_checkout_fields( $fields, 'shipping' );
 	}
-
+	
 	public function alter_checkout_fields( $fields, $form ) {
 		$fields[ $form . '_address_1' ]['class'] = array_merge( $fields[ $form . '_address_1' ]['class'], array( 'form-row-third' ) );
-
-		//$fields[ $form . '_address_1' ]['placeholder'] = 'test';
-
+		
+		
+		$extra_fields = array();
+		
 		if ( ! isset( $fields[ $form . '_house_number' ] ) ) {
-			$fields[ $form . '_house_number' ] = array(
+			$extra_fields[ $form . '_house_number' ] = array(
 				'label'        => __( 'Nr.', 'radish-checkout-fields' ),
 				'required'     => true,
-				'priority'     => 50,
+				'priority'     => 61,
 				'class'        => array( 'form-row-quart-first' ),
 				'autocomplete' => 'number',
 			);
 		}
 		if ( ! isset( $fields[ $form . '_house_number_suffix' ] ) ) {
-			$fields[ $form . '_house_number_suffix' ] = array(
+			$extra_fields[ $form . '_house_number_suffix' ] = array(
 				'label'        => __( 'Suffix', 'radish-checkout-fields' ),
 				'required'     => false,
-				'priority'     => 60,
+				'priority'     => 62,
 				'class'        => array( 'form-row-fifth' ),
 				'autocomplete' => 'suffix',
 			);
 		}
-
-		return $fields;
+		
+		$extra_fields = apply_filters( 'radish_separate_address_fields', $extra_fields );
+		
+		return array_merge( $fields, $extra_fields );
 	}
-
+	
 	public function process_fields( $order_id ) {
-
+		
 		// Default array.
 		$addressData = array(
 			'billing'  => array(
@@ -120,10 +123,10 @@ class Radish_Checkout_Fields {
 				'house_number_suffix' => ''
 			)
 		);
-
+		
 		// Build an array of addressData.
 		foreach ( $addressData as $form => $data ) {
-
+			
 			if ( isset( $_POST[ $form . '_address_1' ] ) && ! empty( $_POST[ $form . '_address_1' ] ) ) {
 				$addressData[ $form ]['street_name'] = sanitize_text_field( $_POST[ $form . '_address_1' ] );
 			}
@@ -134,14 +137,14 @@ class Radish_Checkout_Fields {
 				$addressData[ $form ]['house_number_suffix'] = sanitize_text_field( $_POST[ $form . '_house_number_suffix' ] );
 			}
 		}
-
+		
 		// Loop over again, now with new data.
 		foreach ( $addressData as $form => $data ) {
-
+			
 			$street_name         = $data['street_name'];
 			$house_number        = $data['house_number'];
 			$house_number_suffix = $data['house_number_suffix'];
-
+			
 			// Shipping fields are not always available.
 			if ( $form === 'shipping' ) {
 				if ( empty( $street_name ) ) {
@@ -154,64 +157,64 @@ class Radish_Checkout_Fields {
 					$house_number_suffix = $addressData['billing']['house_number_suffix'];
 				}
 			}
-
+			
 			// Update individual fields.
 			update_post_meta( $order_id, '_' . $form . '_street_name', $street_name );
 			update_post_meta( $order_id, '_' . $form . '_house_number', $house_number );
 			update_post_meta( $order_id, '_' . $form . '_house_number_suffix', $house_number_suffix );
-
+			
 			// Finally combine the data back into address_1.
 			if ( ! empty( $street_name ) && ! empty( $house_number ) ) {
 				update_post_meta( $order_id, '_' . $form . '_address_1', sanitize_text_field( $street_name . ' ' . $house_number . ' ' . $house_number_suffix ) );
 			}
-
+			
 		}
-
-
+		
+		
 	}
-
+	
 	public function show_on_admin_order( $order, $form ) {
 		$street = get_post_meta( $order->get_id(), '_' . $form . '_address_1', true );
 		if ( ! empty( $street ) ) {
 			echo '<p><strong>' . __( 'Address', 'woocommerce' ) . ':</strong> ' . $street . '</p>';
 		}
-
+		
 		$house_number = get_post_meta( $order->get_id(), '_' . $form . '_house_number', true );
 		if ( ! empty( $house_number ) ) {
 			echo '<p><strong>' . __( 'Nr.', 'radish-checkout-fields' ) . ':</strong> ' . $house_number . '</p>';
 		}
-
+		
 		$suffix = get_post_meta( $order->get_id(), '_' . $form . '_house_number_suffix', true );
 		if ( ! empty( $suffix ) ) {
 			echo '<p><strong>' . __( 'Suffix', 'radish-checkout-fields' ) . ':</strong> ' . $suffix . '</p>';
 		}
 	}
-
+	
 	public function show_extra_billing_fields( $order ) {
 		$this->show_on_admin_order( $order, 'billing' );
 	}
-
+	
 	public function show_extra_shipping_fields( $order ) {
 		$this->show_on_admin_order( $order, 'shipping' );
 	}
-
+	
 	public function checkout_move_country_field( $checkout_fields ) {
 		$forms = array( 'billing', 'shipping' );
-
+		
 		foreach ( $forms as $form ) {
 			$checkout_fields[ $form ][ $form . '_country' ]['priority']      = 28;
-			$checkout_fields[ $form ][ $form . '_address_1' ]['placeholder'] = __( 'Street' );
+			$checkout_fields[ $form ][ $form . '_address_1' ]['placeholder'] = __( "Street address", 'woocommerce' );
 		}
-
+		
 		return $checkout_fields;
 	}
-
+	
 	public function checkout_change_address_1_placeholder( $fields ) {
 		$fields['address_1']['placeholder'] = __( 'Street' );
-
+		
 		return $fields;
 	}
-
+	
 }
 
 $radish_checkout_fields = new Radish_Checkout_Fields();
